5096a859d28974273eea6baf496409ddce4609f4
sushen <sushenbiswasaga@gmail.com>
2019-10-11 17:18:42 +0600

    Not Working

--- app/src/main/java/blog/biswas/video/firebasetest/SignupActivity.java
+++ app/src/main/java/blog/biswas/video/firebasetest/SignupActivity.java
@@ -1,14 +1,58 @@
 package blog.biswas.video.firebasetest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import blog.biswas.video.firebasetest.databinding.ActivitySignupBinding;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private ActivitySignupBinding binding;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_signup);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        binding.tvHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignupActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
+        binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signup(binding.editEmail.getText().toString(),binding.editPassword.getText().toString());
+            }
+        });
+    }
+    private void signup(String email, String password){
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(SignupActivity.this, Login.class);
+                    startActivity(intent);
+                }else {
+                    Log.i("RegisterLogin","Fali to creat account");
+                }
+            }
+        });
     }
 }
